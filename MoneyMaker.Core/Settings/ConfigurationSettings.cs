6db46763b83a59d214a7bf8ed511e21bca3b13db
Abdelazez Ramadan <zezo.ramadan77@gmail.com>
2024-04-13 17:52:23 +0200

    Add .gitignore to ignore .vs, .obj, .bin, and debug folders

---
+++ MoneyMaker.Core/Settings/ConfigurationSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyMaker.Core.Settings
+{
+    public class ConfigurationSettings
+    {
+        public ConnectionString ConnectionString { get; set; }
+    }
+}
